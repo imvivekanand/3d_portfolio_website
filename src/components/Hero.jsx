37262979f07353a4df344ae341ab5c7bfbbc9cf6
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-03-08 21:13:50 +0530

    Hero file completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,9 +12,15 @@ const Hero = () => {
       <div className='w-1 sm:h-80 h-40 violet-gradient'/>
       </div>
       <div>
-        <h1>Hi, I'm <span className='text-[#915eff]'>Vivek Anand</span></h1>
+        <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Vivek Anand</span></h1>
+
+        <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          I develop user interfaces, <br className='sm:block hidden'/> MERN stack web applications.
+        </p>
       </div>
+
       </div>
+      <ComputersCanvas/>
     </section>
     )
 }
