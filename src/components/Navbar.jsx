5bc6e90b0dd8ef20812dbb96c933632f6fcb8a1a
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-03-08 09:26:08 +0530

    Navbar created for wide screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import {logo, menu, close} from '../assets';
 
 const Navbar = () => {
 const [active, setActive] = useState('');
+const [toggle, setToggle] = useState(false);
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -20,6 +21,18 @@ const [active, setActive] = useState('');
       <p className='text-white text-[18px] font-bold cursor-pointer'>Vivek Anand <span className='sm:block hidden' >| QualiMe</span></p>
 
       </Link>
+      <ul className="list-none hidden sm:flex flex-row gap-x-10">
+      {navLinks.map((Link) => (
+        <li key={Link.id} className={`${active === Link.title ? "text-white":"text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`} onClick={() => setActive(Link.title)}>
+          <a href={`#${Link.id}`}>{Link.title}</a>
+        </li>
+      ))}
+      </ul>
+
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+      <img src={toggle ? close : menu} alt="menu" className='w-[28px]h-[28px] object-contain cursor-pointer' onClick={()=> setToggle(!toggle)} />
+
+      </div>
 
       </div>
     </nav>
